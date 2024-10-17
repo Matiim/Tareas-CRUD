0a53ac79cb51068b6f377645460fd65492152bb2
Matias Martinez <matimartinezz.927@gmail.com>
2024-10-16 22:54:24 -0300

    subiendo a produccion

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser"
 import authRoutes from './routes/auth-router.js'
 import tasksRouter from './routes/tasks-router.js'
 import cors from 'cors'
+import path from 'path';
 
 const app = express()
 
@@ -22,6 +23,10 @@ app.use(cookieParser())
 
 app.use("/api",authRoutes)
 app.use("/api",tasksRouter)
+
+app.use((req, res, next) => {
+    res.sendFile(path.resolve(__dirname, 'src', 'index.html'));  // Ajusta la ruta al archivo index.html en 'src'
+  });
 
 
 export default app
